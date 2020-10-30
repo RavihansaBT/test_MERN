a9de02480fd696f0f63301545f3fbda8e5de145c
RavihansaBT <ravihansabandara@gmail.com>
2020-10-30 23:54:26 +0530

    check file existancy is fixed

--- bioController.js
+++ bioController.js
@@ -8,19 +8,18 @@ let uploadFolder = "./uploads/";
 let fileName = [];
 
 //for index
-exports.index = (req,res)=> {
+exports.index = (req, res) => {
     Bio.get((err, bio) => {
         if (err)
-        res.json({
-            status: "error",
-            message: err
-        });
+            res.json({
+                status: "error",
+                message: err
+            });
         res.json({
             status: "success",
             message: "Got Bio Successfully!",
-            data: bio       
+            data: bio
         });
-        // console.log(uuidv4();
     });
 };
 
@@ -29,102 +28,91 @@ exports.index = (req,res)=> {
 exports.add = (req, res) => {
     var bio = new Bio();
     bio.bio_id = uuidv4();
-    bio.name = req.body.name? req.body.name : bio.name;
+    bio.name = req.body.name ? req.body.name : bio.name;
     bio.email = req.body.email;
     bio.phone = req.body.phone;
     bio.address = req.body.address;
-    
+
 
     try {
         if (!req.files) {
             res.send({
-                status : false,
-                message : "No files upload"
+                status: false,
+                message: "No files upload"
             });
         }
-        else{
-            let datalist = []; 
-
-            
-
-            // _.forEach(_.keysIn(req.files.photos), (key) => {
-            //     let upload_files = req.files.upload_files[key];
-                
-            //     //move photo to uploads directory
-            //     upload_files.mv(uploadFolder + upload_files.name);
-
-            //     //push file details
-            //     data.push({
-            //         name: upload_files.name,
-            //         mimetype: upload_files.mimetype,
-            //         size: upload_files.size
-            //     });
-            //     fileName = upload_files[key].name;
-            // });
+        else {
+            let datalist = [];
             let upload_files;
-                
-            if(req.files.upload_files.length>1){
+
+            if (req.files.upload_files.length > 1) {
                 fileName = [];
                 _.forEach(_.keysIn(req.files.upload_files), (key) => {
                     upload_files = req.files.upload_files[key];
-                    
-                    //move photo to uploads directory
+
+                    // check file availability
+                    if (fs.existsSync(uploadFolder + upload_files.name)) {
+                        datalist.push(upload_files.name + " already exists");
+                    } else {
+                        //move photo to uploads directory
+                        upload_files.mv(uploadFolder + upload_files.name);
+                        //push file details
+                        datalist.push({
+                            name: upload_files.name,
+                            mimetype: upload_files.mimetype,
+                            size: upload_files.size
+                        });
+
+                        fileName.push(upload_files.name);
+                    }
+                });
+            } else {
+                upload_files = req.files.upload_files;
+
+                if (fs.existsSync(uploadFolder + upload_files.name)) {
+                    datalist.push(upload_files.name + " already exists");
+                } else {
                     upload_files.mv(uploadFolder + upload_files.name);
-    
-                    //push file details
                     datalist.push({
                         name: upload_files.name,
                         mimetype: upload_files.mimetype,
                         size: upload_files.size
                     });
-                    
-                    fileName.push(upload_files.name); 
-                    
-            });
-            } else {
-                upload_files = req.files.upload_files;
-                upload_files.mv(uploadFolder + upload_files.name);
-                datalist.push({
-                    name: upload_files.name,
-                    mimetype: upload_files.mimetype,
-                    size: upload_files.size
-                });
-                fileName = [];
-                fileName.push(upload_files.name); 
+                    fileName = [];
+                    fileName.push(upload_files.name);
+                }
+               
             }
 
             bio.upload_files = fileName;
-            
-            bio.save((err) =>{
-                if(err){
+
+            bio.save((err) => {
+                if (err) {
                     res.json(err);
                 }
-                
-                
-                
+
                 res.send({
-                    status : true,
-                    message : "File uploaded",
-                    data :{
-                        message : "New bio added",
-                        data : bio,
-                        name : datalist
+                    status: true,
+                    message: "File uploaded",
+                    data: {
+                        message: "New bio added",
+                        data: bio,
+                        upload_files_details: datalist
                     }
                 });
 
             });
         }
-        
+
     } catch (error) {
         res.status(500).send(err);
     }
 };
 
-
 //View Bio
 exports.view = (req, res) => {
-    Bio.findById(req.params.bio_id, (err, bio) =>{
-        if(err){
+    Bio.findById(req.params.bio_id, (err, bio) => {
+        if (err) {
             res.send(err);
         }
         res.json({
@@ -136,8 +124,8 @@ exports.view = (req, res) => {
 
 // Update Bio 
 exports.update = (req, res) => {
-    Bio.findById(req.params.bio_id, (err, bio) =>{
-        if(err){
+    Bio.findById(req.params.bio_id, (err, bio) => {
+        if (err) {
             res.send(err);
         }
         bio.name = req.body.name ? req.body.name : bio.name;
@@ -145,9 +133,9 @@ exports.update = (req, res) => {
         bio.phone = req.body.phone;
         bio.address = req.body.address;
 
-//save and check errors
-        bio.save( (err) => {
-            if (err){
+        //save and check errors
+        bio.save((err) => {
+            if (err) {
                 res.json(err)
             }
             res.json({
@@ -159,7 +147,7 @@ exports.update = (req, res) => {
 };
 
 // Delete Bio
-exports.delete =  (req, res) => {
+exports.delete = (req, res) => {
     Bio.deleteOne({
         _id: req.params.bio_id
     }, (err, contact) => {
@@ -178,26 +166,26 @@ exports.uploadFile = async (req, res) => {
         if (!req.files) {
             console.log("test");
             res.send({
-                status : false,
-                message : "No files pload"
+                status: false,
+                message: "No files pload"
             });
         }
-        else{
+        else {
             let avatar = req.files.avatar;
 
             avatar.mv('./uploads/' + avatar.name);
 
             res.send({
-                status : true,
-                message : "File uploaded",
-                data :{
-                    name : avatar.name,
-                    mimetype : avatar.mimetype,
-                    size : avatar.size
+                status: true,
+                message: "File uploaded",
+                data: {
+                    name: avatar.name,
+                    mimetype: avatar.mimetype,
+                    size: avatar.size
                 }
             });
         }
-        
+
     } catch (error) {
         res.status(500).send(err);
     }
